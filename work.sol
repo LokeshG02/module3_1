72bbd7ed6d48c174e0693fe1dc25233afcfa543b
LokeshG02 <guptalokesh2622004@gmail.com>
2024-09-29 14:34:34 +0530

    Update work.sol

--- work.sol
+++ work.sol
@@ -1,42 +1,40 @@
-// write a smart contract that implements the require(), assert() and revert() statements
-// consider a simple school grading system or a small local store. The project doesn't have to be extensive; 
-//just 3-4 functions using all three error handlers will suffice.
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SchoolGrading {
-    address public teacher;
-    uint public grade;
-    bool public gradeSet;
+contract MiniStore {
+    struct Item {
+        string name;
+        uint256 price; 
+        uint256 stock; 
+    }
+
+    mapping(uint256 => Item) public items;
+    uint256 public itemCount;
+    uint256 public totalSales;
 
     constructor() {
-        teacher = msg.sender;
+        addItem("Mango", 5 , 500); 
+        addItem("Apple", 2 , 50);
+        addItem("Guava", 3 , 100);
+        addItem("Kiwi", 10 , 300); 
     }
 
-    modifier onlyTeacher() {
-        require(msg.sender == teacher, "Only teacher can perform this action");
-        _;
+    function addItem(string memory _name, uint256 _price, uint256 _stock) internal {
+        itemCount++;
+        items[itemCount] = Item(_name, _price, _stock);
+        assert(keccak256(abi.encodePacked(items[itemCount].name)) == keccak256(abi.encodePacked(_name)));
     }
 
-    function setGrade(uint newGrade) external onlyTeacher {
-        require(newGrade >= 0 && newGrade <= 100, "Grade must be between 0 and 100");
-        grade = newGrade;
-        gradeSet = true;
+    function buyItem(uint256 _itemId, uint256 _quantity ) public payable {
+        require(_itemId > 0 && _itemId <= itemCount, "Item does not exist.");
+        require(items[_itemId].stock >= _quantity, "Not enough stock.");
+        uint256 totalCost = items[_itemId].price * _quantity;
+        items[_itemId].stock -= _quantity;
+        totalSales += totalCost;
+        assert(totalSales >= totalCost);
     }
 
-   
-
-    function updateTeacher(address newTeacher) external {
-        require(msg.sender == teacher, "Only the current teacher can update");
-        assert(newTeacher != address(0)); // To Check that the new teacher address is not zero
-        teacher = newTeacher;
-    }
-
-    function removeGrade() external onlyTeacher {
-        if (!gradeSet) {
-            revert("Grade has not been set, cannot remove");
-        }
-        grade = 0;
-        gradeSet = false;
+    function refundRequest() public pure {
+        revert("Refunds are not allowed.");
     }
 }
